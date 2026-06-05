96c03c9cec6013d9ea245f778e51a3a157f01408
jose-zenwebmedia <jose@zenwebmedia.com>
2026-06-04 21:36:51 -0400

    Remove wp-staging plugin and staging artifacts

--- wp-content/plugins/wp-staging/Backup/Service/Database/Exporter/RowsExporterProvider.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace WPStaging\Backup\Service\Database\Exporter;
-use WPStaging\Framework\Database\Exporter\AbstractExporterProvider;
-class RowsExporterProvider extends AbstractExporterProvider
-{
-}
